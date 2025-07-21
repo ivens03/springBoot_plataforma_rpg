72369a115d86f5ab22f1e70305ae9f80cda44165
ivens magno <ivensmagno@gmail.com>
2025-07-21 17:33:55 -0300

    style(JogadorRepository / login.html)
    - JogadorRepository remoção de import que não era usado;
    
    - login.html mudança no texto para o usuario;

--- src/main/java/projetos/rpg/domain/repository/JogadorRepository.java
+++ src/main/java/projetos/rpg/domain/repository/JogadorRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import projetos.rpg.domain.model.JogadorModel;
 
+import java.util.Optional;
+
 @Repository
 public interface JogadorRepository extends JpaRepository<JogadorModel, Long> {
 }
